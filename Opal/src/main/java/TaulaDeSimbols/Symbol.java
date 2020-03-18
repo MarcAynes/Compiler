9370fa105912116e38bfbbae51ebf84b270cfb87
Marc Aynes <37773535+MarcAynes@users.noreply.github.com>
2020-03-18 13:06:25 +0100

    add: Symbol Table
    symbol table is created with a hash map

---
+++ Opal/src/main/java/TaulaDeSimbols/Symbol.java
@@ -0,0 +1,19 @@
+package TaulaDeSimbols;
+
+public class Symbol {
+
+    String Reserved_Word;
+    int Type;
+    /*
+    1 - final de linia
+    2 - assignacio
+    3 - operacio
+    4 - declaracio/tipus
+    5 - funcio
+
+     */
+
+    int variable_type;
+
+
+}
