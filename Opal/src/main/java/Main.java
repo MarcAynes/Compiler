b158ea71860967ad965f0ba45bc9d5af04f953cd
claravalls <clara.valls@student.salle.url.edu>
2020-03-19 12:40:45 +0100

    add: parser first stemps. Task LDPG2-24

--- Opal/src/main/java/Main.java
+++ Opal/src/main/java/Main.java
@@ -1,5 +1,17 @@
+import Parser.Parser;
+import TaulaDeSimbols.SymbolTable;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String codi;
+
+        SymbolTable symbolTable = new SymbolTable();
+        Parser parser = new Parser(symbolTable);
+
+        //llegir fitxer codi i passar-ho a string
+        codi = "int a;"; //codi prova
+
+        //inicar Parser
+        parser.syntaxAnalysis(codi);
     }
 }
