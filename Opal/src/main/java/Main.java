947feb600336506f8f553415f928a72a3a1fb26a
Marc Aynes <37773535+MarcAynes@users.noreply.github.com>
2020-03-19 13:21:54 +0100

    LDPG2-19 | Add: test file with some code
    added file with code, and created in the main a little function to read all the tokens that the scanner is creating

--- Opal/src/main/java/Main.java
+++ Opal/src/main/java/Main.java
@@ -1,5 +1,23 @@
+import Scanner.Scanner;
+import TaulaDeSimbols.SymbolTable;
+
+
 public class Main {
+
+    static SymbolTable symbolTable = new SymbolTable();
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(args[0]);
+        Scanner s = new Scanner(args[0]);
+
+        for(int i = 0; i < 50; i++) {
+            String token = s.getToken();
+            if (token != null) {
+                System.out.println(token);
+            }else{
+                System.out.println("final");
+                break;
+            }
+        }
     }
 }
